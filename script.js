7f0067b675aad8a2a9c07f7f7e0c8aa5165134ca
ReyesSanti <127696692+ReyesSanti@users.noreply.github.com>
2023-08-26 16:46:57 -0500

    Funcionalidad
    logre que dichos datos agregados se agregaran a la tabla

--- script.js
+++ script.js
@@ -7,17 +7,23 @@ formulario.addEventListener("submit", function(event) {
     const nombre = document.getElementById("nombre").value;
     const edad = document.getElementById("edad").value;
     const sexo = document.getElementById("sexo").value;
+    const correo = document.getElementById("correo").value;
+    const celular = document.getElementById("celular").value;
+    const cumpleaños = document.getElementById("cumpleanos").value;
     
-    agregarFilaATabla(nombre, edad, sexo);
+    agregarFilaATabla(nombre, edad, sexo, correo, celular, cumpleaños);
     formulario.reset();
 });
 
-function agregarFilaATabla(nombre, edad, sexo) {
+function agregarFilaATabla(nombre, edad, sexo, correo, celular, cumpleaños) {
     const fila = document.createElement("tr");
     fila.innerHTML = `
         <td>${nombre}</td>
         <td>${edad}</td>
         <td>${sexo}</td>
+        <td>${correo}</td>
+        <td>${celular}</td>
+        <td>${cumpleaños}</td>
     `;
     tablaCuerpo.appendChild(fila);
 }
