ac3276b9fe2f06b06af91652528dab52205abab7
ReyesSanti <127696692+ReyesSanti@users.noreply.github.com>
2023-08-26 16:21:14 -0500

    Arreglar los cuadros de texto

--- script.js
+++ script.js
@@ -6,16 +6,18 @@ formulario.addEventListener("submit", function(event) {
     
     const nombre = document.getElementById("nombre").value;
     const edad = document.getElementById("edad").value;
+    const sexo = document.getElementById("sexo").value;
     
-    agregarFilaATabla(nombre, edad);
+    agregarFilaATabla(nombre, edad, sexo);
     formulario.reset();
 });
 
-function agregarFilaATabla(nombre, edad) {
+function agregarFilaATabla(nombre, edad, sexo) {
     const fila = document.createElement("tr");
     fila.innerHTML = `
         <td>${nombre}</td>
         <td>${edad}</td>
+        <td>${sexo}</td>
     `;
     tablaCuerpo.appendChild(fila);
 }
